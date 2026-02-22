e4fc14f736095a2f860305375329c32997405754
Exampleuser274 <alexanderkovach6@gmail.com>
2026-02-22 11:51:11 -0700

    Started loadFile

--- src/main/Files.java
+++ src/main/Files.java
@@ -1,5 +1,38 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import problemdomain.Vehicle;
+
 public class Files {
 //Class for save & load functions
+//Alexander Kovach is working on load function
+	public ArrayList<Vehicle> loadFile() {
+		ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+		File vehicleListFile = new File("res\\vehicles.txt");
+		try (Scanner vehicleReader = new Scanner(vehicleListFile)){
+			String line;
+			String[] parts;
+			long carID;
+			String vehicleType;
+			String subType;
+			int speed;
+			double fuel;
+			int seats;
+			int year;
+			String drivetrain;
+			int price;
+			while (vehicleReader.hasNextLine()) {
+				line = vehicleReader.nextLine();
+				parts = line.split(";");
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return vehicleList;
+	}
 }
