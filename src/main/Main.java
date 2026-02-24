97aee7bfa4c68fa199ed1fb9d9c78261cbc1d1ba
Exampleuser274 <alexanderkovach6@gmail.com>
2026-02-23 18:47:29 -0700

    added imports to main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.util.ArrayList;
+
+import problemdomain.*;
+
 public class Main {
 //Main loop only
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
--- src/main/Menu.java
+++ src/main/Menu.java
@@ -36,12 +36,57 @@ public class Menu {
 		return(vehicleList);
 	}
 	public void typeSearch(ArrayList<Vehicle> vehicleList) {
-		
+		String userSearch;
+		boolean searchCheck = false;
+		try (Scanner myScanner = new Scanner(System.in)) {
+			System.out.println("Enter the type of vehicle");
+			userSearch = myScanner.nextLine();
+		}
+		for (Vehicle car:vehicleList) {
+			if (car.getVehicleType() == userSearch) {
+				System.out.println(car);
+				searchCheck = true;
+			}
+		}
+		if (searchCheck == false) {
+			System.out.println("No vehicles found");
+		}
 	}
 	public void subtypeSearch(ArrayList<Vehicle> vehicleList) {
+		String userType;
+		String userSubType;
+		boolean searchCheck = false;
+		try (Scanner myScanner = new Scanner(System.in)) {
+			System.out.println("Enter the type of vehicle");
+			userType = myScanner.nextLine();
+			System.out.println("Enter the subtype");
+			userSubType = myScanner.nextLine();
+		}
+		for (Vehicle car:vehicleList) {
+			if (car.getVehicleType() == userType && userSubType == car.getSubtype()) {
+				System.out.println(car);
+				searchCheck = true;
+			}
+		}
+		if (searchCheck == false) {
+			System.out.println("No vehicles found");
+		}
 		
 	}
 	public void randomSearch(ArrayList<Vehicle> vehicleList) {
-		
+		int vehicleAmount;
+		try (Scanner myScanner = new Scanner(System.in)) {
+			System.out.println("Enter the number of vehicles");
+			vehicleAmount = myScanner.nextInt();
+		}
+		ArrayList<Vehicle> randomVehicles = new ArrayList<Vehicle>(vehicleList);
+		java.util.Collections.shuffle(randomVehicles);
+		if (vehicleAmount > randomVehicles.size()) {
+			vehicleAmount = randomVehicles.size();
+		}
+		for (int index = 0; index < vehicleAmount; index++) {
+			Vehicle car = randomVehicles.get(index);
+			System.out.println(car);
+		}
 	}
 }
